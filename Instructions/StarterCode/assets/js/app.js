47f38e259351cf4a7438c0af20888431a0f08c40
MBerkley25 <mberkley25@gmail.com>
2020-09-28 11:37:01 -0500

    structure

--- Instructions/StarterCode/assets/js/app.js
+++ Instructions/StarterCode/assets/js/app.js
@@ -1,1 +1,63 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth = 900;
+var svgHeight = 500;
+
+var margin = { top: 20, right: 40, bottom: 80, left: 100 };
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Append an SVG group
+var chart = svg.append("g");
+
+// Append a div to the body to create tooltips, assign it a class
+d3.select(".chart").append("div").attr("class", "tooltip").style("opacity", 0);
+
+// Retrieve data from the CSV file and execute everything below
+d3.csv("data.csv", function(err, myData) {
+  if (err) throw err;
+
+  myData.forEach(function(data) {
+    data.obese = Number(data.obese);
+    data.bachelorOrHigher = Number(data.bachelorOrHigher);
+    data.currentSmoker = Number(data.currentSmoker);
+  });
+
+  console.log(myData);
+
+  // Create scale functions
+  var yLinearScale = d3.scaleLinear().range([height, 0]);
+
+  var xLinearScale = d3.scaleLinear().range([0, width]);
+
+  // Create axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // Variables store minimum and maximum values in a column in data.csv
+  var xMin;
+  var xMax;
+  var yMax;
+
+  // Function identifies the minimum and maximum values in a column in data.csv
+  // and assigns them to xMin and xMax variables, which defines the axis domain
+  function findMinAndMax(dataColumnX) {
+    xMin = d3.min(myData, function(data) {
+      return Number(data[dataColumnX]) * 0.8;
+    });
+
+    xMax = d3.max(myData, function(data) {
+      return Number(data[dataColumnX]) * 1.1;
+    });
+
+    yMax = d3.max(myData, function(data) {
+      return Number(data.bachelorOrHigher) * 1.1;
